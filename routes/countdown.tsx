9802756be259812c9063eae2b41ff983c2e4ee33
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-10-18 19:16:18 +0000

    Formatted files

--- routes/countdown.tsx
+++ routes/countdown.tsx
@@ -1,11 +1,11 @@
-import Countdown from '../islands/Countdown.tsx';
+import Countdown from "../islands/Countdown.tsx";
 
 export default function CountdownPage() {
-    const date = new Date();
-    date.setHours(date.getHours() + 1);
-    return (
-        <p>
-            The big event will start in <Countdown target={date.toISOString()} />
-        </p>
-    )
+  const date = new Date();
+  date.setHours(date.getHours() + 1);
+  return (
+    <p>
+      The big event will start in <Countdown target={date.toISOString()} />
+    </p>
+  );
 }
