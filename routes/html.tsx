6c32e2ee7315c772b03ea78b919b3032aae7e63c
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-10-18 20:36:38 +0000

    Twind stuff

--- routes/html.tsx
+++ routes/html.tsx
@@ -1,5 +1,5 @@
 import { PageProps } from "$fresh/server.ts";
 
 export default function Page(props: PageProps) {
-  return <div>You are on the page '{props.url.href}'</div>;
+  return <div class="text-center font-bold align-middle">You are on the page '{props.url.href}'</div>;
 }
