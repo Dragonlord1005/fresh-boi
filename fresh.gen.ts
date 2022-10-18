5b51d7d15b9d2d29188583d0248cf8d5bee95662
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-10-18 18:50:07 +0000

    stuff

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,16 +5,20 @@
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/about.tsx";
 import * as $1 from "./routes/api/joke.ts";
-import * as $2 from "./routes/greet/[name].tsx";
-import * as $3 from "./routes/index.tsx";
+import * as $2 from "./routes/api/random-uuid.ts";
+import * as $3 from "./routes/github/[username].tsx";
+import * as $4 from "./routes/greet/[name].tsx";
+import * as $5 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
     "./routes/about.tsx": $0,
     "./routes/api/joke.ts": $1,
-    "./routes/greet/[name].tsx": $2,
-    "./routes/index.tsx": $3,
+    "./routes/api/random-uuid.ts": $2,
+    "./routes/github/[username].tsx": $3,
+    "./routes/greet/[name].tsx": $4,
+    "./routes/index.tsx": $5,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
