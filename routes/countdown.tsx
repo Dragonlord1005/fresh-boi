183edf204f47bc2cbaeb57020ff343c1c4b3f2a0
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-10-18 19:04:03 +0000

    ITS THE FINAL COUNTDOWN!!!

---
+++ routes/countdown.tsx
@@ -0,0 +1,11 @@
+import Countdown from '../islands/Countdown.tsx';
+
+export default function CountdownPage() {
+    const date = new Date();
+    date.setHours(date.getHours() + 1);
+    return (
+        <p>
+            The big event will start in <Countdown target={date.toISOString()} />
+        </p>
+    )
+}
