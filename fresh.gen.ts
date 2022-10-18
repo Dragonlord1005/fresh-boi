4721f27f871d5ceba5dcf5b40e97813b83ded763
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-10-18 13:20:09 -0600

    Do the tutorial (#2)
    * stuff
    
    * ITS THE FINAL COUNTDOWN!!!
    
    * Formatted files

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,19 +5,27 @@
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/about.tsx";
 import * as $1 from "./routes/api/joke.ts";
-import * as $2 from "./routes/greet/[name].tsx";
-import * as $3 from "./routes/index.tsx";
-import * as $$0 from "./islands/Counter.tsx";
+import * as $2 from "./routes/api/random-uuid.ts";
+import * as $3 from "./routes/countdown.tsx";
+import * as $4 from "./routes/github/[username].tsx";
+import * as $5 from "./routes/greet/[name].tsx";
+import * as $6 from "./routes/index.tsx";
+import * as $$0 from "./islands/Countdown.tsx";
+import * as $$1 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
     "./routes/about.tsx": $0,
     "./routes/api/joke.ts": $1,
-    "./routes/greet/[name].tsx": $2,
-    "./routes/index.tsx": $3,
+    "./routes/api/random-uuid.ts": $2,
+    "./routes/countdown.tsx": $3,
+    "./routes/github/[username].tsx": $4,
+    "./routes/greet/[name].tsx": $5,
+    "./routes/index.tsx": $6,
   },
   islands: {
-    "./islands/Counter.tsx": $$0,
+    "./islands/Countdown.tsx": $$0,
+    "./islands/Counter.tsx": $$1,
   },
   baseUrl: import.meta.url,
   config,
